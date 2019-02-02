2a2d169057a5ed4c00bdcb1f04e43e65afa72f2f
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2019-02-02 16:10:33 +0100

    Fix background to foreground error

--- minifeed/AppDelegate.swift
+++ minifeed/AppDelegate.swift
@@ -25,11 +25,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func applicationWillEnterForeground(_ application: UIApplication) {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    HomeController.instance?.reloadData()
+    Flash.progress()
   }
 
   func applicationDidBecomeActive(_ application: UIApplication) {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    HomeController.instance?.reloadData()
   }
 
   func applicationWillTerminate(_ application: UIApplication) {
