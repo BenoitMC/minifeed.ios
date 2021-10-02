cd04a91d0f876430980f9e7f3363bc903870b49e
Benoit MARTIN-CHAVE <benoit@martin-chave.com>
2021-10-02 18:05:42 +0200

    iOS 15 compatibility

--- minifeed/Commons/Style.swift
+++ minifeed/Commons/Style.swift
@@ -3,7 +3,18 @@ import UIKit
 
 enum Style {
   static func applyCommons() {
+    let navbarAppearance = UINavigationBarAppearance()
+    navbarAppearance.configureWithOpaqueBackground()
     UINavigationBar.appearance().isTranslucent = false
+    UINavigationBar.appearance().standardAppearance = navbarAppearance
+    UINavigationBar.appearance().scrollEdgeAppearance = navbarAppearance
+
+    let toolbarAppearance = UIToolbarAppearance()
+    toolbarAppearance.configureWithOpaqueBackground()
     UIToolbar.appearance().isTranslucent = false
+    UIToolbar.appearance().standardAppearance = toolbarAppearance
+    if #available(iOS 15.0, *) {
+      UIToolbar.appearance().scrollEdgeAppearance = toolbarAppearance
+    }
   }
 }
